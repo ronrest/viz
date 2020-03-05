f3ac89ac2d785d54f66a40b7e7e8e5e1facbf2f9
ronrest <ronny.coding@gmail.com>
2020-03-05 18:09:05 +1100

    FEAT: fix lineplot

--- standard/lineplot.py
+++ standard/lineplot.py
@@ -1,17 +1,22 @@
-from . import plt
+from . import plt, pd
 
-def lineplot(x, figsize=(10, 6), title="title", ax=None, color="#307EC7", label=""):
+def lineplot(x, y=None, figsize=(10, 6), title="title", ax=None, color="#307EC7", label="", xlabel="x", ylabel="y", **kwargs):
     if ax is None:
         fig, ax = plt.subplots(figsize=figsize)
         fig.suptitle(title, fontsize=15)
     else:
         fig = ax.get_figure()
 
-    ax.plot(x, color=color,  label=label)
+    if y is None:
+        y = pd.Series(x)
+        x = y.index
 
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.legend(loc="lower right", title="", frameon=False,  fontsize=8)
+    ax.plot(x, y, color=color,  label=label, **kwargs)
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    if label != "":
+        ax.legend(loc="lower right", title="", frameon=False,  fontsize=8)
     # ax.set_aspect('equal')
     ax.grid(True, which='both')
 
