0c2ec7900a230d3b34d29df62b1496f110da0572
ronrest <ronny.coding@gmail.com>
2020-03-07 11:17:13 +1100

    FEAT: update distplot

--- distributions/distplot.py
+++ distributions/distplot.py
@@ -1,6 +1,6 @@
 from . import plt, sns, pd, np
 
-def distplot(x, figsize=(10, 6), bins=None, title="title", ax=None, color="steelblue", label="KDE", legend_pos="lower right", **kwargs):
+def distplot(x, figsize=(10, 6), bins=None, title="title", ax=None, color="steelblue", label="KDE", xlabel="x", ylabel="density", legend_pos="lower right", **kwargs):
     # potentially embed in another figure
     if ax is None:
         fig, ax = plt.subplots(figsize=figsize)
@@ -12,20 +12,30 @@ def distplot(x, figsize=(10, 6), bins=None, title="title", ax=None, color="steel
     x = pd.Series(x)
     x = x[x.notnull()]
 
+
+    # Settings
+    kde_kws = dict({"color": color, "lw": 2, "label": label}, **(kwargs.get("kde_kws", {})))
+    hist_kws=dict({"histtype": "step", "linewidth": 1, "alpha": 0.5, "color": color}, **(kwargs.get("hist_kws", {})))
+    for key in ["kde_kws", "hist_kws"]:
+        try:
+            kwargs.pop(key)
+        except:
+            pass
+
     # plot
     sns.set_style('whitegrid')
     ax = sns.distplot(x,
         # rug=True, rug_kws={"color": color}, # Show rugplot
         bins=bins,
-        kde_kws={"color": color, "lw": 2, "label": label},
-        hist_kws={"histtype": "step", "linewidth": 1, "alpha": 0.5, "color": color},
+        kde_kws=kde_kws,
+        hist_kws=hist_kws,
         ax=ax,
         **kwargs
         )
 
     # format
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
     ax.legend(loc=legend_pos, title="", frameon=False,  fontsize=8)
     # ax.set_aspect('equal')
     # ax.grid(True, which='both')
