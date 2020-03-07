a4208a67fc692b29bc91be3fe4c673e0ffedc23e
ronrest <ronny.coding@gmail.com>
2020-03-08 10:13:57 +1100

    FEAT: update imports

--- __init__.py
+++ __init__.py
@@ -6,4 +6,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from . standard.lineplot import lineplot
+from . standard import lineplot
+from . standard import barplot
+from . standard import stepplot
+from . standard import scatterplot
+
+def create_figax(grid=(1,1), figsize=(10,6), title=""):
+    fig, axes = plt.subplots(grid[0],grid[1], figsize=figsize)
+    axes = np.array(axes).flatten()
+    fig.suptitle(title, fontsize=15)
+    plt.close()
+    return fig, axes
--- standard/__init__.py
+++ standard/__init__.py
@@ -1,7 +1,15 @@
-import pandas as pd
-import numpy as np
+from .. import pd
+from .. import np
+# import pandas as pd
+# import numpy as np
 
 # import matplotlib as mpl
 # mpl.use('TkAgg') # graphical external window
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+from . lineplot import lineplot
+from . barplot import barplot
+from . stepplot import stepplot
+from . scatterplot import scatterplot
